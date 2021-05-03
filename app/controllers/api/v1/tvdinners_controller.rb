29dd1c38a3b71fa6667d3f50535bcbdc807366c1
Dex <fitch.dexter@gmail.com>
2021-05-03 12:13:52 -0700

    Backend for tvdinner loading

--- app/controllers/api/v1/tvdinners_controller.rb
+++ app/controllers/api/v1/tvdinners_controller.rb
@@ -1,15 +1,22 @@
 module Api 
   module V1
     class TvdinnersController < ApplicationController
-      before_action :authenticate_user!, :except => [:index]
+      before_action :authenticate_user!, except: [:index]
 
       def index
         render json: Tvdinner.all
       end
 
       def mytvdinners
-        render json: current_user.tvdinners
+        if current_user
+          render json: current_user.tvdinners
+        else
+          render json: {}, status: 401
+        end 
       end
+
+
+    
     end
   end
 end
