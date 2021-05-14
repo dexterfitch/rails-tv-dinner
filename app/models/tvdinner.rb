2d57664a8c616ed2ea2ce11aa44d529274080a3b
Dex <fitch.dexter@gmail.com>
2021-05-14 12:12:12 -0700

    Add ar validations to tvdinners

--- app/models/tvdinner.rb
+++ app/models/tvdinner.rb
@@ -1,3 +1,7 @@
 class Tvdinner < ApplicationRecord
   belongs_to :user
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :film_id, presence: true
+  validates :recipe_ids, presence: true
 end
